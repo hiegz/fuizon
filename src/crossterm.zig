8bfa3a8c53f8e6d73c9cf2ba529cb4463f66d8bf
Volodymyr But <vbut.dev+git@gmail.com>
2025-01-27 21:40:26 +0100

    feat: add functions for controlling the alternate screen

--- src/crossterm.zig
+++ src/crossterm.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const fuizon = @import("fuizon.zig");
 const c = @import("headers.zig").c;
 
@@ -54,4 +55,62 @@ pub fn disableRawMode() error{BackendError}!void {
     var ret: c_int = undefined;
     ret = c.crossterm_disable_raw_mode();
     if (0 != ret) return error.BackendError;
+}
+
+/// A backend implementation that uses crossterm to render to the terminal.
+pub fn Backend(WriterType: type) type {
+    return struct {
+        const Self = @This();
+
+        allocator: std.mem.Allocator,
+        writer: *WriterType,
+        stream: c.crossterm_stream,
+
+        /// Creates a new `CrosstermBackend` instance attached to the specified writer.
+        pub fn init(allocator: std.mem.Allocator, writer: WriterType) std.mem.Allocator.Error!Self {
+            var backend: Self = undefined;
+            backend.allocator = allocator;
+            backend.writer = try allocator.create(WriterType);
+            backend.writer.* = writer;
+            backend.stream = .{
+                .context = @ptrCast(backend.writer),
+                .write_fn = Self._write,
+                .flush_fn = Self._flush,
+            };
+            return backend;
+        }
+
+        /// Releases all allocated memory.
+        pub fn deinit(self: Self) void {
+            self.allocator.destroy(self.writer);
+        }
+
+        /// Switches to the alternate screen.
+        pub fn enterAlternateScreen(self: *Self) error{BackendError}!void {
+            var ret: c_int = undefined;
+            ret = c.crossterm_enter_alternate_screen(&self.stream);
+            if (0 != ret) return error.BackendError;
+        }
+
+        /// Switches back to the main screen.
+        pub fn leaveAlternateScreen(self: *Self) error{BackendError}!void {
+            var ret: c_int = undefined;
+            ret = c.crossterm_leave_alternate_screen(&self.stream);
+            if (0 != ret) return error.BackendError;
+        }
+
+        /// ...
+        fn _write(buf: [*c]const u8, buflen: usize, context: ?*anyopaque) callconv(.C) c_long {
+            const maxlen = @as(usize, std.math.maxInt(c_long));
+            const len = if (buflen <= maxlen) buflen else maxlen;
+            const ctx: *WriterType = @ptrCast(@alignCast(context));
+            return @intCast(ctx.write(buf[0..len]) catch return -1);
+        }
+
+        /// ...
+        fn _flush(context: ?*anyopaque) callconv(.C) c_int {
+            _ = context;
+            return 0;
+        }
+    };
 }
