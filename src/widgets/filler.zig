5d5079f13ee70e5907ca8fc17449560b71d65289
Volodymyr But <git@hiegz.com>
2025-02-17 17:32:55 +0100

    extend the filler interface to allow configuration of its content

--- src/widgets/filler.zig
+++ src/widgets/filler.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const fuizon = @import("../fuizon.zig");
 
 const Attributes = fuizon.style.Attributes;
+const Style = fuizon.style.Style;
 const Color = fuizon.style.Color;
 
 const Area = fuizon.area.Area;
@@ -9,7 +10,9 @@ const Frame = fuizon.frame.Frame;
 const FrameCell = fuizon.frame.FrameCell;
 
 pub const Filler = struct {
-    color: fuizon.style.Color,
+    content: u21 = ' ',
+    width: u2 = 1,
+    style: Style = .{},
 
     /// Renders the filler within a section of the specified frame,
     /// as defined by the provided area.
@@ -17,14 +20,9 @@ pub const Filler = struct {
         for (area.left()..area.right()) |x| {
             for (area.top()..area.bottom()) |y| {
                 const cell = frame.index(@intCast(x), @intCast(y));
-                cell.width = 1;
-                cell.content = ' ';
-                cell.style = .{
-                    .foreground_color = .default,
-                    .background_color = self.color,
-
-                    .attributes = Attributes.none,
-                };
+                cell.width = self.width;
+                cell.content = self.content;
+                cell.style = self.style;
             }
         }
     }
@@ -32,20 +30,27 @@ pub const Filler = struct {
 
 test "Filler.render()" {
     const expected_frame = try Frame.initContent(std.testing.allocator, &[_][]const u8{
-        "   ",
-        "   ",
-        "   ",
+        "---",
+        "---",
+        "---",
     }, .{
-        .foreground_color = .default,
+        .foreground_color = .black,
         .background_color = .white,
-        .attributes = Attributes.none,
+        .attributes = Attributes.all,
     });
     defer expected_frame.deinit();
 
     var actual_frame = try Frame.initArea(std.testing.allocator, expected_frame.area);
     defer actual_frame.deinit();
 
-    (Filler{ .color = .white }).render(&actual_frame, actual_frame.area);
+    (Filler{
+        .content = '-',
+        .style = .{
+            .foreground_color = .black,
+            .background_color = .white,
+            .attributes = Attributes.all,
+        },
+    }).render(&actual_frame, actual_frame.area);
 
     try std.testing.expectEqualSlices(
         FrameCell,
