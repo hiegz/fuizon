21b5e293584d47a97bb3a31641a25d147f7f78c6
Volodymyr But <git@hiegz.com>
2025-03-06 15:34:42 +0100

    add a text widget

--- src/widgets.zig
+++ src/widgets.zig
@@ -1,5 +1,6 @@
 pub const container = @import("widgets/container.zig");
 pub const filler = @import("widgets/filler.zig");
+pub const text = @import("widgets/text.zig");
 
 test "widgets" {
     @import("std").testing.refAllDecls(@This());
