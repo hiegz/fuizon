9ea5f1adfef8458c9bf090bc99f0a4fc9bd9a2b8
Volodymyr But <git@hiegz.com>
2025-03-24 23:07:41 +0100

    rewrite the text api

--- src/fuizon.zig
+++ src/fuizon.zig
@@ -1,12 +1,12 @@
 // zig fmt: off
-pub const event      = @import("event.zig");
-pub const keyboard   = @import("keyboard.zig");
-pub const style      = @import("style.zig");
-pub const backend    = @import("backend.zig");
-pub const frame      = @import("frame.zig");
-pub const layout     = @import("layout.zig");
-
-pub const widgets    = @import("widgets.zig");
+pub const event    = @import("event.zig");
+pub const keyboard = @import("keyboard.zig");
+pub const style    = @import("style.zig");
+pub const backend  = @import("backend.zig");
+pub const frame    = @import("frame.zig");
+pub const layout   = @import("layout.zig");
+pub const text     = @import("text.zig");
+pub const widgets  = @import("widgets.zig");
 // zig fmt: on
 
 test "fuizon" {
