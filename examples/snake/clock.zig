279a3c2a2bf54e2ed6d7f38129def5b7ca740a47
Volodymyr But <vbut.dev+git@gmail.com>
2025-03-27 23:16:47 +0100

    snake-demo: change the clock run/pause logic

--- examples/snake/clock.zig
+++ examples/snake/clock.zig
@@ -31,33 +31,12 @@ pub const Clock = struct {
         self.timer.deinit();
     }
 
-    pub fn reset(self: *Clock) void {
-        self.startTimer();
+    pub fn pause(self: *Clock) void {
+        self.state = .idle;
     }
 
     pub fn run(self: *Clock) void {
-        if (self.live())
-            return;
         self.state = .live;
-        self.startTimer();
-    }
-
-    pub fn pause(self: *Clock) void {
-        if (self.idle())
-            return;
-        self.state = .idle;
-        self.cancelTimer();
-    }
-
-    pub fn live(self: Clock) bool {
-        return self.state == .live;
-    }
-
-    pub fn idle(self: Clock) bool {
-        return self.state == .idle;
-    }
-
-    fn startTimer(self: *Clock) void {
         self.timer.reset(
             self.loop,
             &self.c,
@@ -74,13 +53,11 @@ pub const Clock = struct {
                 ) xev.CallbackAction {
                     _ = l;
                     _ = c;
-                    _ = r catch |err| {
-                        std.debug.assert(err == error.Canceled);
-                        ud.?.state = .idle;
+                    _ = r catch
                         return .disarm;
-                    };
-                    std.debug.assert(ud.?.state == .live);
-                    ud.?.startTimer();
+                    if (ud.?.state == .idle)
+                        return .disarm;
+                    ud.?.run();
                     ud.?.callback();
                     return .disarm;
                 }
@@ -88,28 +65,12 @@ pub const Clock = struct {
         );
     }
 
-    fn cancelTimer(self: *Clock) void {
-        self.timer.cancel(
-            self.loop,
-            &self.c,
-            &self.c_cancel,
-            Clock,
-            self,
-            (struct {
-                fn callback(
-                    ud: ?*Clock,
-                    l: *xev.Loop,
-                    c: *xev.Completion,
-                    r: xev.Timer.CancelError!void,
-                ) xev.CallbackAction {
-                    _ = l;
-                    _ = c;
-                    _ = r catch unreachable;
-                    std.debug.assert(ud.?.state == .idle);
-                    return .disarm;
-                }
-            }).callback,
-        );
+    pub fn live(self: Clock) bool {
+        return self.state == .live;
+    }
+
+    pub fn idle(self: Clock) bool {
+        return self.state == .idle;
     }
 };
 
