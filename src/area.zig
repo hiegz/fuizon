f834b00b444a69875a40d14874a053a7e9eaf52d
Volodymyr But <git@hiegz.com>
2025-09-12 19:14:15 +0200

    inline origin fields

--- src/area.zig
+++ src/area.zig
@@ -1,57 +1,60 @@
 const std = @import("std");
-const fuizon = @import("fuizon.zig");
-const Coordinate = fuizon.Coordinate;
 
 pub const Area = struct {
     width: u16,
     height: u16,
-    origin: Coordinate,
+    x: u16,
+    y: u16,
 
     pub fn top(self: Area) u16 {
-        return self.origin.y;
+        return self.y;
     }
 
     test "top() should return the topmost coordinate" {
         try std.testing.expectEqual(5, (Area{
             .width = 5,
             .height = 9,
-            .origin = .{ .x = 1, .y = 5 },
+            .x = 1,
+            .y = 5,
         }).top());
     }
 
     pub fn bottom(self: Area) u16 {
-        return self.height + self.origin.y;
+        return self.height + self.y;
     }
 
     test "bottom() should return the bottommost coordinate" {
         try std.testing.expectEqual(14, (Area{
             .width = 5,
             .height = 9,
-            .origin = .{ .x = 1, .y = 5 },
+            .x = 1,
+            .y = 5,
         }).bottom());
     }
 
     pub fn left(self: Area) u16 {
-        return self.origin.x;
+        return self.x;
     }
 
     test "left() should return the leftmost coordinate" {
         try std.testing.expectEqual(1, (Area{
             .width = 5,
             .height = 9,
-            .origin = .{ .x = 1, .y = 5 },
+            .x = 1,
+            .y = 5,
         }).left());
     }
 
     pub fn right(self: Area) u16 {
-        return self.width + self.origin.x;
+        return self.width + self.x;
     }
 
     test "right() should return the rightmost coordinate" {
         try std.testing.expectEqual(6, (Area{
             .width = 5,
             .height = 9,
-            .origin = .{ .x = 1, .y = 5 },
+            .x = 1,
+            .y = 5,
         }).right());
     }
 };
