403f9d7b64b5574d6569333b391335ceb1239f02
Volodymyr But <git@hiegz.com>
2025-09-15 17:33:32 +0200

    fix the rgb demo

--- examples/rgb.zig
+++ examples/rgb.zig
@@ -3,6 +3,7 @@ const fuizon = @import("fuizon");
 
 fn prompt(comptime p: []const u8, reader: *std.io.Reader, writer: *std.io.Writer) !u8 {
     try writer.print(p, .{});
+    try writer.flush();
     const slice = try reader.takeDelimiterExclusive('\n');
     return try std.fmt.parseInt(u8, slice, 10);
 }
@@ -12,27 +13,30 @@ pub fn main() !void {
     var stdin_reader = std.fs.File.stdin().reader(&read_buffer);
     const reader = &stdin_reader.interface;
 
-    var write_buffer: [0]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&write_buffer);
-    const writer = &stdout_writer.interface;
+    const writer = fuizon.getWriter();
 
     const r: u8 = prompt("Red: ", reader, writer) catch {
         try writer.print("Invalid input\n", .{});
+        try writer.flush();
         return;
     };
     const g: u8 = prompt("Green: ", reader, writer) catch {
         try writer.print("Invalid input\n", .{});
+        try writer.flush();
         return;
     };
     const b: u8 = prompt("Blue: ", reader, writer) catch {
         try writer.print("Invalid input\n", .{});
+        try writer.flush();
         return;
     };
 
     try writer.print("\n\r", .{});
     try writer.print("Your color: ", .{});
-    try fuizon.backend.text.background.set(writer, .{ .rgb = .{ .r = r, .g = g, .b = b } });
+    try fuizon.setBackground(fuizon.Rgb(r, g, b));
     try writer.print("            ", .{});
-    try fuizon.backend.text.background.set(writer, .default);
+    try fuizon.setBackground(.default);
     try writer.print("\n\r", .{});
+
+    try writer.flush();
 }
