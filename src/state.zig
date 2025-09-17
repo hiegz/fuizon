0a230e60e48c26732648e2ba40ad8a0d4084fcaa
Volodymyr But <git@hiegz.com>
2025-09-17 15:43:35 +0200

    move global state to state.zig

---
+++ src/state.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const posix = std.posix;
+const windows = @import("windows.zig");
+
+pub var buffer: []u8 = &.{};
+pub var writer: ?std.fs.File.Writer = null;
+
+pub var original_mode: switch (builtin.os.tag) {
+    .macos, .linux => posix.termios,
+    .windows => struct { out: windows.DWORD, in: windows.DWORD },
+    else => unreachable,
+} = undefined;
