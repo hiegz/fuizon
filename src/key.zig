80b918823ae5650390ca0c2d900180497c795503
Volodymyr But <git@hiegz.com>
2025-09-18 18:24:47 +0200

    introduce Key struct

--- src/key.zig
+++ src/key.zig
@@ -1,5 +1,22 @@
 const std = @import("std");
 
+pub const Key = struct {
+    code: KeyCode,
+    modifiers: KeyModifiers,
+
+    pub fn init(code: KeyCode, modifiers: KeyModifiers) Key {
+        return .{ .code = code, .modifiers = modifiers };
+    }
+
+    pub fn initChar(char: u21, modifiers: KeyModifiers) Key {
+        return .{ .code = .{ .char = char }, .modifiers = modifiers };
+    }
+
+    pub fn format(self: Key, writer: *std.io.Writer) !void {
+        try writer.print("Key {{ code: {f}, modifiers: {f} }}", .{ self.code, self.modifiers });
+    }
+};
+
 pub const KeyModifier = enum(u3) {
     // zig fmt: off
     shift   = 1 << 0,
