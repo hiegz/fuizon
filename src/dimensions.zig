03ae2f5c22a49f2610b394c112bc2de2cc7796e0
Volodymyr But <git@hiegz.com>
2025-09-23 16:19:20 +0200

    add dimensions constructor

--- src/dimensions.zig
+++ src/dimensions.zig
@@ -1,4 +1,8 @@
 pub const Dimensions = struct {
     width: u16,
     height: u16,
+
+    pub fn init(width: u16, height: u16) Dimensions {
+        return .{ .width = width, .height = height };
+    }
 };
