ed988565bbf09e5311dfac39649c09f3e58d7710
Volodymyr But <git@hiegz.com>
2025-09-23 10:04:08 +0200

    add Span.raw() and Span.styled()

--- src/span.zig
+++ src/span.zig
@@ -4,7 +4,11 @@ pub const Span = struct {
     content: []const u8 = "",
     style: Style = .{},
 
-    pub fn init(content: []const u8, style: Style) Span {
+    pub fn raw(content: []const u8) Span {
+        return .{ .content = content, .style = .{} };
+    }
+
+    pub fn styled(content: []const u8, style: Style) Span {
         return .{ .content = content, .style = style };
     }
 };
