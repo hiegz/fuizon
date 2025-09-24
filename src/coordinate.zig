8a957fc02851d23d1cafc196bb5a32ce0cff28a5
Volodymyr But <git@hiegz.com>
2025-09-24 19:04:08 +0200

    add coordinate constructor

--- src/coordinate.zig
+++ src/coordinate.zig
@@ -1,1 +1,10 @@
-pub const Coordinate = struct { x: u16, y: u16 };
+pub const Coordinate = struct {
+    x: u16,
+    y: u16,
+
+    pub fn init(x: u16, y: u16) Coordinate {
+        return .{ .x = x, .y = y };
+    }
+
+    pub const coord = init;
+};
