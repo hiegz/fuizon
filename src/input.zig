2526c7b722db0ac1e3f068d08810da76df8add3a
Volodymyr But <git@hiegz.com>
2025-09-26 00:59:00 +0200

    add fuizon.read()

--- src/input.zig
+++ src/input.zig
@@ -3,8 +3,16 @@ const Key = @import("key.zig").Key;
 
 pub const Input = union(enum) {
     key: Key,
+    resize,
+    eof,
 
     pub fn format(self: Input, writer: *std.Io.Writer) !void {
-        try writer.print("Input {{ {f} }}", .{self.key});
+        try switch (self) {
+            // zig fmt: off
+            .key    => |key| writer.print("Input {{ {f} }}", .{key}),
+            .resize =>       writer.print("Input {{ resize }}", .{}),
+            .eof    =>       writer.print("Input {{ eof }}", .{}),
+            // zig fmt: on
+        };
     }
 };
---
+++ src/posix.zig
@@ -0,0 +1,103 @@
+const std = @import("std");
+const terminal = @import("terminal.zig");
+const Input = @import("input.zig").Input;
+const InputParser = @import("input_parser.zig").InputParser;
+
+// zig fmt: off
+
+/// Flag to indicate that a SIGWINCH signal interrupted.
+///
+/// Only relevant inside fuizon.read() (see below)
+var winch: std.atomic.Value(bool) = .init(false);
+
+fn sighandler(signo: c_int) callconv(.c) void {
+    if (signo == std.posix.SIG.WINCH) {
+        winch.store(true, .release);
+    }
+}
+
+pub fn read(maybe_timeout: ?u32) error{ NotATerminal, ReadFailed, Interrupted, Unexpected }!?Input {
+    const input:  std.posix.fd_t     = try terminal.getInputHandle();
+    var   ret:    usize              = undefined;
+    var   byte:   u8                 = undefined;
+    var   parser: InputParser        = .{};
+    var   result: InputParser.Result = .none;
+
+    // Install the SIGWINCH signal handler.
+    var oldact: std.posix.Sigaction = undefined;
+    var newact: std.posix.Sigaction = undefined;
+    newact.handler = .{ .handler = sighandler };
+    newact.mask = std.posix.sigemptyset();
+    newact.flags = 0;
+    std.posix.sigaction(std.posix.SIG.WINCH, &newact, &oldact);
+    defer std.posix.sigaction(std.posix.SIG.WINCH, &oldact, null);
+
+    // Block SIGWINCH from now on.
+    //
+    // Generally, we don’t want SIGWINCH to interrupt reading.
+    //
+    // The only exception is when we’re waiting for user input;
+    // in that case, a window resize event is also relevant.
+    var oldset: std.posix.sigset_t = undefined;
+    var newset: std.posix.sigset_t = std.posix.sigemptyset();
+    std.posix.sigaddset(&newset, std.posix.SIG.WINCH);
+    std.posix.sigprocmask(std.posix.SIG.BLOCK, &newset, &oldset);
+    defer std.posix.sigprocmask(std.posix.SIG.SETMASK, &oldset, null);
+
+    var fds: [1]std.posix.pollfd = undefined;
+    fds[0].fd = input;
+    fds[0].events = std.posix.POLL.IN;
+
+    var timespec:   std.posix.timespec = undefined;
+    var timeout:  ?*std.posix.timespec = null;
+
+    if (maybe_timeout) |to| {
+        timeout       = &timespec;
+        timespec.sec  = @intCast((to / 1000));
+        timespec.nsec = @intCast((to % 1000) * std.time.ns_per_ms);
+    }
+
+    ret = std.posix.ppoll(&fds, timeout, &oldset) catch |err| switch (err) {
+        error.SignalInterrupt => return {
+            if (winch.load(.acquire) == false)
+                return error.Interrupted;
+            winch.store(false, .release);
+            return .resize;
+        },
+        else => return error.ReadFailed,
+    };
+
+    // timeout expired
+    if (ret == 0)
+        return null;
+
+    std.debug.assert(ret == 1);
+
+    timeout = &timespec;
+    timespec.sec  = 0;
+    timespec.nsec = 0;
+
+    while (true) {
+        if (fds[0].revents & std.posix.POLL.IN == 0)
+            return error.ReadFailed;
+
+        ret = std.posix.read(input, @ptrCast(&byte)) catch return error.ReadFailed;
+        if (ret == 0) return error.ReadFailed;
+
+        result = try parser.step(byte);
+
+        if (result == .final)
+            return result.final;
+
+        ret = std.posix.ppoll(&fds, timeout, null) catch |err| switch (err) {
+            error.SignalInterrupt => return error.Interrupted,
+            else                  => return error.ReadFailed,
+        };
+
+        if (result == .ambiguous and ret == 0)
+            return result.ambiguous;
+
+        if (result == .none and ret == 0)
+            return error.ReadFailed;
+    }
+}
