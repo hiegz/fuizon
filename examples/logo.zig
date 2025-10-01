3e86d863019e3ea097abf840d6e20e5a2c7b6513
Volodymyr But <git@hiegz.com>
2025-10-01 19:44:40 +0200

    restore the logo demo

--- examples/logo.zig
+++ examples/logo.zig
@@ -1,115 +1,130 @@
 const std = @import("std");
 const fuizon = @import("fuizon");
-const Area = fuizon.Area;
-const Color = fuizon.Color;
-const Rgb = fuizon.Rgb;
 
-pub fn center(area: Area, width: u16, height: u16) Area {
-    std.debug.assert(width <= area.width and height <= area.height);
-    var centered = @as(Area, undefined);
-    centered.width = width;
-    centered.height = height;
-    centered.x = area.x + (area.width - width) / 2;
-    centered.y = area.y + (area.height - height) / 2;
-    return centered;
-}
+const Logo = struct {
+    // zig fmt: off
 
-// zig fmt: off
-const red:         Color = Rgb(250,  22, 50);
-const light_red:   Color = Rgb(250,  81, 50);
-const dark_yellow: Color = Rgb(250, 207, 14);
-const yellow:      Color = Rgb(250, 227, 13);
-const orange:      Color = Rgb(250, 130,  8);
-// zig fmt: on
+    const WIDTH:  u16 = 7 * 2;
+    const HEIGHT: u16 = 10;
 
-const WIDTH: u16 = 6 * 2;
-const HEIGHT: u16 = 10;
+    const red:         fuizon.Character = .init(' ', .init(.default, .Rgb(250,  22, 50), .none));
+    const light_red:   fuizon.Character = .init(' ', .init(.default, .Rgb(250,  81, 50), .none));
+    const dark_yellow: fuizon.Character = .init(' ', .init(.default, .Rgb(250, 207, 14), .none));
+    const yellow:      fuizon.Character = .init(' ', .init(.default, .Rgb(250, 227, 13), .none));
+    const orange:      fuizon.Character = .init(' ', .init(.default, .Rgb(250, 130,  8), .none));
 
-const Cell = struct { x: u16, y: u16, color: Color };
-fn cell(x: u16, y: u16, color: Color) Cell {
-    return .{ .x = x * 2, .y = y, .color = color };
-}
+    // zig fmt: on
 
-const map = [_]Cell{
-    cell(2, 0, red),
-    cell(0, 1, red),
-    cell(6, 2, red),
+    canvas: fuizon.Buffer,
 
-    cell(3, 2, red),
-    cell(3, 3, red),
-    cell(4, 3, red),
-    cell(2, 4, red),
-    cell(1, 5, red),
-    cell(0, 6, red),
-    cell(0, 7, red),
-    cell(0, 8, red),
-    cell(1, 9, red),
-    cell(5, 5, red),
-    cell(5, 6, red),
-    cell(5, 7, red),
-    cell(4, 8, red),
-    cell(3, 9, red),
+    pub fn init(gpa: std.mem.Allocator) error{OutOfMemory}!Logo {
+        var self: Logo = undefined;
+        self.canvas = try fuizon.Buffer.initDimensions(gpa, .init(Logo.WIDTH, Logo.HEIGHT));
 
-    cell(4, 7, light_red),
-    cell(4, 6, light_red),
-    cell(4, 5, light_red),
-    cell(4, 4, light_red),
-    cell(3, 4, light_red),
-    cell(2, 5, light_red),
-    cell(1, 6, light_red),
+        self.draw(2, 0, red);
+        self.draw(0, 1, red);
+        self.draw(6, 2, red);
 
-    cell(2, 6, orange),
-    cell(1, 7, orange),
-    cell(3, 8, orange),
+        self.draw(3, 2, red);
+        self.draw(3, 3, red);
+        self.draw(4, 3, red);
+        self.draw(2, 4, red);
+        self.draw(1, 5, red);
+        self.draw(0, 6, red);
+        self.draw(0, 7, red);
+        self.draw(0, 8, red);
+        self.draw(1, 9, red);
+        self.draw(5, 5, red);
+        self.draw(5, 6, red);
+        self.draw(5, 7, red);
+        self.draw(4, 8, red);
+        self.draw(3, 9, red);
 
-    cell(1, 8, dark_yellow),
-    cell(2, 9, dark_yellow),
-    cell(3, 5, dark_yellow),
-    cell(3, 6, dark_yellow),
+        self.draw(4, 7, light_red);
+        self.draw(4, 6, light_red);
+        self.draw(4, 5, light_red);
+        self.draw(4, 4, light_red);
+        self.draw(3, 4, light_red);
+        self.draw(2, 5, light_red);
+        self.draw(1, 6, light_red);
 
-    cell(3, 7, yellow),
-    cell(2, 7, yellow),
-    cell(2, 8, yellow),
+        self.draw(2, 6, orange);
+        self.draw(1, 7, orange);
+        self.draw(3, 8, orange);
+
+        self.draw(1, 8, dark_yellow);
+        self.draw(2, 9, dark_yellow);
+        self.draw(3, 5, dark_yellow);
+        self.draw(3, 6, dark_yellow);
+
+        self.draw(3, 7, yellow);
+        self.draw(2, 7, yellow);
+        self.draw(2, 8, yellow);
+
+        return self;
+    }
+
+    pub fn deinit(
+        self: Logo,
+        gpa: std.mem.Allocator,
+    ) void {
+        self.canvas.deinit(gpa);
+    }
+
+    pub fn measure(
+        self: Logo,
+        opts: fuizon.Widget.MeasureOptions,
+    ) anyerror!fuizon.Dimensions {
+        _ = self;
+
+        return .init(
+            @min(opts.max_width, Logo.WIDTH),
+            @min(opts.max_height, Logo.HEIGHT),
+        );
+    }
+
+    pub fn render(
+        self: Logo,
+        buffer: *fuizon.Buffer,
+        area: fuizon.Area,
+    ) anyerror!void {
+        var container = fuizon.Container.empty;
+        container.child = self.canvas.widget();
+        try container.render(buffer, area);
+    }
+
+    fn draw(self: *Logo, x: anytype, y: anytype, character: fuizon.Character) void {
+        self.canvas.set(x * 2, y, character);
+        self.canvas.set(x * 2 + 1, y, character);
+    }
 };
 
 pub fn main() !void {
-    try fuizon.init(std.heap.page_allocator, 1024, .stdout);
-    defer fuizon.deinit(std.heap.page_allocator) catch unreachable;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
 
-    defer fuizon.getWriter().flush() catch {};
+    try fuizon.init();
+    defer fuizon.deinit() catch {};
 
-    try fuizon.enableRawMode();
-    defer fuizon.disableRawMode() catch {};
     try fuizon.enterAlternateScreen();
     defer fuizon.leaveAlternateScreen() catch {};
-    try fuizon.hideCursor();
-    defer fuizon.showCursor() catch {};
+
+    const logo = try Logo.init(allocator);
+    defer logo.deinit(allocator);
 
     while (true) {
-        const screen = try fuizon.getScreenSize();
-        try fuizon.moveCursorTo(0, 0);
-        for (0..screen.width * screen.height) |_| {
-            try fuizon.getWriter().print(" ", .{});
-        }
+        try fuizon.render(&logo, .fullscreen, .{});
 
-        if (WIDTH <= screen.width and HEIGHT <= screen.height) {
-            const art_area = center(Area.init(screen.width, screen.height, 0, 0), WIDTH, HEIGHT);
-            for (map) |map_cell| {
-                const x = art_area.x + map_cell.x;
-                const y = art_area.y + map_cell.y;
-
-                try fuizon.moveCursorTo(x, y);
-                try fuizon.setBackground(map_cell.color);
-                try fuizon.getWriter().print("  ", .{});
-                try fuizon.setBackground(.default);
-            }
-        }
-
-        try fuizon.getWriter().flush();
-
-        switch (try fuizon.event.read()) {
-            .key => return,
-            else => {},
+        const event = try fuizon.readInput(.{});
+        if (event == null)
+            break;
+        switch (event.?) {
+            .key => |k| if (k.code == .char and k.code.char == 'q') break,
+            .eof => break,
+            .resize => continue,
         }
     }
+
+    try fuizon.clear();
 }
