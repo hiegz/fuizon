0a732e54f76175ccb0c2b986a44a8beac120c200
Volodymyr But <git@hiegz.com>
2025-10-10 18:24:28 +0200

    move cassowary.Expression to expression.zig

--- src/cassowary.zig
+++ src/cassowary.zig
@@ -21,6 +21,7 @@
 const std = @import("std");
 const float32 = @import("float32.zig");
 
+const Expression = @import("expression.zig").Expression;
 const Variable = @import("variable.zig").Variable;
 const Term = @import("term.zig").Term;
 const Operator = @import("operator.zig").Operator;
@@ -844,57 +845,6 @@ pub const Constraint = struct {
 
     pub fn deinit(self: *const Constraint, gpa: std.mem.Allocator) void {
         self.expression.deinit(gpa);
-    }
-};
-
-pub const Expression = struct {
-    constant:  f32 = 0.0,
-    term_list: std.ArrayList(Term) = .empty,
-
-    pub const empty = Expression{};
-
-    pub fn deinit(self: *const Expression, gpa: std.mem.Allocator) void {
-        gpa.free(self.term_list.allocatedSlice());
-    }
-
-    pub fn clearAndRetainCapacity(self: *Expression) void {
-        self.constant = 0.0;
-        self.term_list.clearRetainingCapacity();
-    }
-
-    pub fn add(
-        self: *Expression,
-        gpa: std.mem.Allocator,
-        coefficient: f32,
-        variable: *Variable,
-    ) error{OutOfMemory}!void {
-        if (float32.nearZero(coefficient)) return;
-        try self.term_list.append(gpa, Term.init(coefficient, variable));
-    }
-
-    pub fn sub(
-        self: *Expression,
-        gpa: std.mem.Allocator,
-        coefficient: f32,
-        variable: *Variable,
-    ) error{OutOfMemory}!void {
-        try self.add(gpa, -coefficient, variable);
-    }
-
-    pub fn mul(self: *Expression, k: f32) void {
-        self.constant *= k;
-        for (self.term_list.items) |*term|
-            term.coefficient *= k;
-    }
-
-    pub fn append(
-        self: *Expression,
-        gpa: std.mem.Allocator,
-        expression: Expression,
-    ) error{OutOfMemory}!void {
-        self.constant += expression.constant;
-        for (expression.term_list.items) |term|
-            try self.add(gpa, term.coefficient, term.variable);
     }
 };
 
