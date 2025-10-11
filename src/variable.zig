321f99cd8e754f32fbb67d5f37be0a78e50cb9f7
Volodymyr But <git@hiegz.com>
2025-10-11 16:48:54 +0200

    add Variable.create()/destroy() methods

--- src/variable.zig
+++ src/variable.zig
@@ -10,6 +10,20 @@ pub const Variable = struct {
         return .{ .name = name };
     }
 
+    /// Allocates and initializes a new `Variable` using the given allocator.
+    /// The caller owns the returned instance and must call `Variable.destroy`
+    /// to free it.
+    pub fn create(gpa: std.mem.Allocator, name: []const u8) error{OutOfMemory}!*Variable {
+        const self: *Variable = try gpa.create(Variable);
+        self.* = .init(name);
+        return self;
+    }
+
+    /// Frees all resources allocated by `Variable.create` using the given allocator.
+    pub fn destroy(self: *Variable, gpa: std.mem.Allocator) void {
+        gpa.destroy(self);
+    }
+
     pub fn id(self: *const Variable) usize {
         return @intFromPtr(self);
     }
